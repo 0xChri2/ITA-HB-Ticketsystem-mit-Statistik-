0c2838a275fb16edd8d7ae98c304e470df3c4692
Filp xd <filipmarin35@gmail.com>
2021-10-06 11:02:56 +0200

    foked

--- ITA-HB-Ticketsystem-mit-Statistik/pages/Kontakte.php
+++ ITA-HB-Ticketsystem-mit-Statistik/pages/Kontakte.php
@@ -3,7 +3,7 @@
     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="de" xml:lang="de">
 	 <head>
-		  <title>Allround Handwerkbetrieb</title>
+		  <title>Maurer</title>
 		  <meta http-equiv="content-type" content="text/plain; charset=utf-8" />
 		  <meta http-equiv="content-language" content="de" />
 		  <link type = "text/css" rel = "stylesheet" href = "../style.css" />
--- ITA-HB-Ticketsystem-mit-Statistik/pages/forum.php
+++ ITA-HB-Ticketsystem-mit-Statistik/pages/forum.php
@@ -2,13 +2,36 @@
     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="de" xml:lang="de">
 	 <head>
-		  <title>Handwerker | Forum</title>
+		  <title>Maurer | Forum</title>
 		  <meta http-equiv="content-type" content="text/plain; charset=utf-8" />
 		  <meta http-equiv="content-language" content="de" />
-		  <link type = "text/css" rel = "stylesheet" href = "style.css" />
+		  <link type = "text/css" rel = "stylesheet" href = "../style.css" />
 	</head>
 
     <body>
-        
+    <div id="heading">
+            <h1> Allround Handwerkerbetrieb! </h1>
+
+
+        </div>
+        <div id="menu">
+            <ul>
+                <li class="topmenu">
+                    <a href=""><img src="pictures/Drei Striche.png"></a>
+                    
+                    <ul>
+                    <li class="submenu"><a href="index.php">Startseite</a></li>
+                        <li class="submenu"><a href="pages/Über uns.php">Über uns</a></li>
+						<li class="submenu"><a href="pages/Ticketsystem.php">Tätigkeiten</a></li>
+						<li class="submenu"><a href="pages/Forum.php">FAQ</a></li>
+						<li class="submenu"><a href="pages/Kontakte.php">Kontakte</a></li>
+						<li class="submenu"><a href="pages/Impressum.php">Impressum</a></li>
+						
+                    </ul>
+                </li>
+            </ul>
+        </div>
+
+
     </body>
 </html>

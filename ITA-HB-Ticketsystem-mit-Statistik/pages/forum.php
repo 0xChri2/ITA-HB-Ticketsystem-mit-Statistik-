8168ffb4ccd23f4d7d90a95b300883d7ae207047
ficko90 <filipmarin35@gmail.com>
2021-10-06 22:35:17 +0200

    Schritt 1

--- ITA-HB-Ticketsystem-mit-Statistik/pages/forum.php
+++ ITA-HB-Ticketsystem-mit-Statistik/pages/forum.php
@@ -5,33 +5,12 @@
 		  <title>Maurer | Forum</title>
 		  <meta http-equiv="content-type" content="text/plain; charset=utf-8" />
 		  <meta http-equiv="content-language" content="de" />
-		  <link type = "text/css" rel = "stylesheet" href = "../style.css" />
+		  <link type = "text/css" rel = "stylesheet" href = "../styles.css" />
 	</head>
 <!-- dakro-->
     <body>
-    <div id="heading">
-            <h1> Allround Handwerkerbetrieb! </h1>
-
-
+        <div id="background">
+            hi
         </div>
-        <div id="menu">
-            <ul>
-                <li class="topmenu">
-                    <a href=""><img src="pictures/Drei Striche.png"></a>
-                    
-                    <ul>
-                    <li class="submenu"><a href="index.php">Startseite</a></li>
-                        <li class="submenu"><a href="pages/Über uns.php">Über uns</a></li>
-						<li class="submenu"><a href="pages/Ticketsystem.php">Tätigkeiten</a></li>
-						<li class="submenu"><a href="pages/Forum.php">FAQ</a></li>
-						<li class="submenu"><a href="pages/Kontakte.php">Kontakte</a></li>
-						<li class="submenu"><a href="pages/Impressum.php">Impressum</a></li>
-						
-                    </ul>
-                </li>
-            </ul>
-        </div>
-
-
     </body>
 </html>

d0b5960323a10cd74a4dace68fd8caac97a16361
ImChri2 <chris@chris-industries.de>
2021-10-06 10:37:46 +0200

    Add files via upload

---
+++ ITA-HB-Ticketsystem-mit-Statistik/ITA-HB-Ticketsystem-mit-Statistik/pages/Forum.php
@@ -0,0 +1,50 @@
+<!-- ImChri2 -->
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="de" xml:lang="de">
+	 <head>
+		  <title>Allround Handwerkbetrieb</title>
+		  <meta http-equiv="content-type" content="text/plain; charset=utf-8" />
+		  <meta http-equiv="content-language" content="de" />
+		  <link type = "text/css" rel = "stylesheet" href = "../style.css" />
+	</head>
+
+<body bgcolor="#101010">
+    
+        <div id="heading">
+        <h1> Allround Handwerkerbetrieb! </h1>
+
+        </div>
+        <div id="menu">
+            <ul>
+                <li class="topmenu">
+                    <a href=""><img src="../pictures/Drei Striche.png"></a>
+                    <ul>
+                        <li class="submenu"><a href="../index.php">Startseite</a></li>
+						<li class="submenu"><a href="Ticketsystem.php">Ticketsystem</a></li>
+						<li class="submenu"><a href="Forum.php">Forum</a></li>
+						<li class="submenu"><a href="Kontakte.php">Kontakte</a></li>
+						<li class="submenu"><a href="Impressum.php">Impressum</a></li>
+                    </ul>
+                </li>
+                
+            </ul>
+        </div>
+        <div id="banner">
+        </div>
+        <div id="article">
+        <center>
+                <h1>Forum</h1>
+                <h4>Hier soll ein Forum erstellen.</h4>
+        </center>
+        </div>
+
+
+        <div id="footer">
+              <a href="Impressum.php">Impressum</a> | <a href="Kontakte.php"> Kontakt </a>
+        </div>
+
+   
+</body>
+</html>
+
---
+++ ITA-HB-Ticketsystem-mit-Statistik/ITA-HB-Ticketsystem-mit-Statistik/pages/Impressum.php
@@ -0,0 +1,57 @@
+<!-- ImChri2 -->
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="de" xml:lang="de">
+	 <head>
+		  <title>Maurer</title>
+		  <meta http-equiv="content-type" content="text/plain; charset=utf-8" />
+		  <meta http-equiv="content-language" content="de" />
+		  <link type = "text/css" rel = "stylesheet" href = "../style.css" />
+	</head>
+
+<body bgcolor="#101010">
+    
+        <div id="heading">
+        <h1> Maurer </h1>
+
+        </div>
+        <div id="menu">
+            <ul>
+                <li class="topmenu">
+                    <a href=""><img src="../pictures/Drei Striche.png"></a>
+                    <ul>
+                        <li class="submenu"><a href="../index.php">Startseite</a></li>
+						<li class="submenu"><a href="Ticketsystem.php">Ticketsystem</a></li>
+						<li class="submenu"><a href="Forum.php">Forum</a></li>
+						<li class="submenu"><a href="Kontakte.php">Kontakte</a></li>
+						<li class="submenu"><a href="Impressum.php">Impressum</a></li>
+                    </ul>
+                </li>
+                
+            </ul>
+        </div>
+        <div id="banner">
+        </div>
+        <div id="article">
+        <center>
+               <h1>Impressum</h1>
+               <br />
+               <h4>Geschäftsführer: ImChri2 und Bertyy</h4>
+               <h4>Standort: XXXXXXXX</h4>
+               <h4>40210 Düsseldorf</h4>
+               <h4>Telefon: 1234567890</h4>
+               <h4>E-Mail: AllroundHandwerker@Handwerk.de</h4>
+               <h4>Umsatzsteueridentifikationsnummer: DE1234525262114164437878</h4>
+               
+        </center>
+        </div>
+
+
+        <div id="footer">
+              <a href="Impressum.php">Impressum</a> | <a href="Kontakte.php"> Kontakt </a>
+        </div>
+
+   
+</body>
+</html>
+

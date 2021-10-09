ea0e9eb79a766872d9f3016415860aa0012bce62
ficko90 <filipmarin35@gmail.com>
2021-10-09 17:59:38 +0200

    Login-container design

--- ITA-HB-Ticketsystem-mit-Statistik/pages/forum.php
+++ ITA-HB-Ticketsystem-mit-Statistik/pages/forum.php
@@ -1,16 +1,34 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="de" xml:lang="de">
-	 <head>
-		  <title>Maurer | Forum</title>
-		  <meta http-equiv="content-type" content="text/plain; charset=utf-8" />
-		  <meta http-equiv="content-language" content="de" />
-		  <link type = "text/css" rel = "stylesheet" href = "../styles.css" />
-	</head>
+
+<head>
+    <title>Maurer | Forum</title>
+    <meta http-equiv="content-type" content="text/plain; charset=utf-8" />
+    <meta http-equiv="content-language" content="de" />
+    <link type="text/css" rel="stylesheet" href="../forumstyle.css" />
+</head>
 <!-- dakro-->
-    <body>
-        <div id="background">
-            hi
+
+<body>
+    <div id="background">
+        <div class="logincontainer">
+            <h1>LOGIN</h1>
+
+            <form>
+                <div class="input-container">
+                    <input type="text" required="" />
+                    <label>Full Name</label>
+                </div>
+
+                <div class="input-container">
+                    <input type="mail" required="" />
+                    <label>Email</label>
+                </div>
+
+                <button type="button" class="btn">submit</button>
+            </form>
         </div>
-    </body>
+    </div>
+</body>
+
 </html>

c544a6dce83de2524528a883f4d9871170ea4dc2
ficko90 <filipmarin35@gmail.com>
2021-12-05 20:44:37 +0100

    added new homepage & fixed forum.php

--- ITA-HB-Ticketsystem-mit-Statistik/pages/forum.php
+++ ITA-HB-Ticketsystem-mit-Statistik/pages/forum.php
@@ -97,10 +97,6 @@
                         }
                         fclose($s);
 
-                        /*
-                            Beim Erstellen von Ticket, email & username in users.csv speichern+
-                        */
-
                         if($logged == true)
                         {
                             echo '<center><div class="success-box">';
@@ -139,7 +135,7 @@
                 if(isset($_POST['thread']))
                 {
                     $eintrag = htmlspecialchars($_POST['eintrag']);
-                    $date = date('d.m.y');
+                    $date = date('d.m.Y');
 
                     $e = fopen("../data/temp/tempuser.txt", "r");
 
@@ -148,10 +144,9 @@
                     $mail = substr($textline, strpos($textline, "\t"));
 
                     fclose($e);
-                    //WORK WITH TIMESTAMPS HERE
                     $f = fopen("../data/forum-data/threads.csv", "a");
-                    $data = array($user, $eintrag, $date, (time() / 60 / 60), $mail);
-                    fputcsv($f, $data, ";");
+                    $data = array($user, str_replace("\r\n", "<br />", $eintrag), $date, time(), $mail);
+                    fputcsv($f, $data, ";", chr(127));
                     fclose($f);
                     echo '<center><div class="success-box">';
                     echo 'ERFOLG!';
@@ -165,36 +160,67 @@
 <!-- ToDo: show $thread_timestamp_info only in hour of the day -->
 </div>
 <?php
-    $today = time();
-    $thread_timestamp = time() - time();
+$t = fopen("../data/forum-data/threads.csv", "r");
+$today = time();
+while(!feof($t))
+{
+    $row = fgets($t, 4096);
+    $column = explode(";", $row);
     $thread_timestamp_info = "";
 
-    if($thread_timestamp == 0)
+    if(sizeof($column) == 5)
     {
-        $thread_timestamp_info = "~ vor ein paar Sekunden";
-    }
+        //$days = floor(($today / 60 / 60 / 24) - ($column[3] / 60 / 60 / 24));
+        $hours = floor(($today / 60 / 60) - ($column[3] / 60 / 60));
+        $minutes = floor(($today / 60) - ($column[3] / 60));
+        //echo $days . "<br>";
+        //echo $hours . "<br>";
+        //echo $minutes . "<br>";
+        if($hours == 0 && $minutes == 0)
+        {
+            $thread_timestamp_info = " ~ vor ein paar Sekunden";
+        }
+    
+        elseif($hours == 0)
+        {
+            $thread_timestamp_info = " ~ vor " . $minutes . " Minuten";
+            if($minutes == 1)
+            {
+                $thread_timestamp_info = " ~ vor eine Minute";
+            }
+        }
 
-    echo '<div id="forum-section">
+        elseif($hours > 0 && $hours < 24)
+        {
+            $thread_timestamp_info = " ~ vor " . $hours . " Stunden";
+            if($hours == 1)
+            {
+                $thread_timestamp_info = " ~ vor eine Stunde";
+            }
+        }
+    
+        echo '<div id="forum-section">
         <div class="thread-box">
             <img src="../pictures/profile.jpg" alt="profile" class="user-picture">
-            <div class="user">
-                2021-12-01-08:56-MUS-MAX-679
+            <div class="user">'.
+                $column[0] . '
             </div>
-
+    
             <div class="thread-timestamp">' .
                  $thread_timestamp_info . '<br><br>
             </div>
-
-            <div class="thread-date">
-                10.11.2021
+    
+            <div class="thread-date">'.
+                $column[2].'
             </div>
-            <div class="thread">
-            Anstatt ein Programm mit vielen Anweisungen zur Ausgabe von HTML zu schreiben, schreibt man etwas HTML und bettet einige Anweisungen ein, die irgendetwas tun (wie hier "Hallo, ich bin ein PHP-Skript!" auszugeben). Der PHP-Code steht zwischen speziellen Anfangs- und Abschluss-Verarbeitungsinstruktionen, mit denen man in den "PHP-Modus" und zurück wechseln kann.
-
-PHP unterscheidet sich von clientseitigen Sprachen wie Javascript dadurch, dass der Code auf dem Server ausgeführt wird und dort HTML-Ausgaben generiert, die an den Client gesendet werden. Der Client erhält also nur das Ergebnis der Skriptausführung, ohne dass es möglich ist herauszufinden, wie der eigentliche Code aussieht. Sie können Ihren Webserver auch anweisen, alle Ihre HTML-Dateien mit PHP zu parsen, denn dann gibt es wirklich nichts, das dem Benutzer sagt, was Sie in petto haben.
+            <div class="thread">'.
+                $column[1] . '
             </div>
         </div>
     </div>';
+    }
+}
+fclose($t);
 ?>
 
     <div id="footer">
